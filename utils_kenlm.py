445ff7fed095033ca3a5e21c430fda11471c3f94
uyoung-jeong <farandole09@unist.ac.kr>
2018-01-17 13:50:15 +0900

    add files

---
+++ utils_kenlm.py
@@ -0,0 +1,43 @@
+"""
+These codes are originally from ARAE utils.py
+https://github.com/jakezhaojb/ARAE?files=1
+Some parts are modified
+"""
+import os
+def load_kenlm():
+    global kenlm
+    import kenlm
+
+def train_ngram_lm(kenlm_path, data_path, output_path, N):
+    """
+    Trains a modified Kneser-Ney n-gram KenLM from a text file.
+    Creates a .arpa file to store n-grams.
+    """
+    # create .arpa file of n-grams
+    curdir = os.path.abspath(os.path.curdir)
+    #
+    command = "bin/lmplz -o "+str(N)+" <"+os.path.join(curdir, data_path) + \
+              " >"+os.path.join(curdir, output_path)
+    os.system("cd "+os.path.join(kenlm_path, 'build')+" && "+command)
+
+    load_kenlm()
+    # create language model
+    model = kenlm.Model(output_path)
+
+    return model
+
+
+def get_ppl(lm, sentences):
+    """
+    Assume sentences is a list of strings (space delimited sentences)
+    """
+    total_nll = 0
+    total_wc = 0
+    for sent in sentences:
+        words = sent.strip().split()
+        score = lm.score(sent, bos=True, eos=False)
+        word_count = len(words)
+        total_wc += word_count
+        total_nll += score
+    ppl = 10**-(total_nll/total_wc)
+    return ppl
