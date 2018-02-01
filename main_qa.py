2aac0d994d15c2deffae3209ab020abbd0ab5489
uyoung-jeong <farandole09@unist.ac.kr>
2018-02-01 21:52:32 +0900

    added more files

---
+++ main_qa.py
@@ -0,0 +1,46 @@
+import logging
+import os
+
+from loader.book_corpus import BookCorpusDataset
+from loader.preprocess import preprocess_data_vocab, preprocess_simpleqa, split_simple_questions
+from test.test import test
+from train.train import train
+from train.network import Network
+from utils.parser import parser
+from utils.utils import Config, set_logger, prepare_paths
+
+log = logging.getLogger('main')
+
+if __name__ == '__main__':
+    # Parsing arguments and set configs
+    args = parser.parse_args()
+    cfg = Config(vars(args))
+
+    # Set all the paths
+    prepare_paths(cfg)
+
+    # Logger
+    set_logger(cfg)
+    log = logging.getLogger('main')
+
+    # split simple_questions dataset file
+    split_simple_questions()
+
+    # Preprocessing
+    preprocess_simpleqa(cfg)
+    vocab = preprocess_vocab(cfg)
+
+    # Load dataset
+    train_q_data = BookCorpusDataset(cfg.train_q_data_filepath)
+    train_a_data = BookCorpusDataset(cfg.train_a_data_filepath)
+    # Build network
+    net = Network(cfg, train_q_data, train_a_data, vocab)
+
+    # Train
+    if not cfg.test:
+        train(net)
+    # Test
+    else:
+        test(net)
+
+    log.info('End of program.')
